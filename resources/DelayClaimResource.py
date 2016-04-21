8d0e1046216db0c32b55e5e7ef50b7f7747c043c
Alejandro Saucedo <axsauze@gmailcom>
2016-04-21 17:44:11 +0100

    added params holding

--- resources/DelayClaimResource.py
+++ resources/DelayClaimResource.py
@@ -1,39 +1,8 @@
-from flask_restful import Resource, reqparse, marshal_with, fields
+from flask_restful import Resource, reqparse, marshal_with, fields, request
 
 from models import Journey, DelayClaim
 
-payment_fields = {
-    'paymentType': fields.String,
-    'accountNumber': fields.String,
-    'sortcode': fields.String
-}
-
-contact_details = {
-    'title': fields.String,
-    'firstName': fields.String,
-    'lastName': fields.String,
-    'address': fields.String,
-    'email': fields.String,
-    'postcode': fields.String
-}
-
-delay_claim = {
-    'fromStation': fields.String,
-    'toStation': fields.String,
-    'ticketClass': fields.String,
-    'ticketType': fields.String,
-    'fromDate': fields.DateTime,
-    'toDate': fields.DateTime,
-    'journeyDate': fields.DateTime,
-    'cost': fields.Float
-}
-
-user_fields = {
-    'image64': fields.String,
-    'delayClaim': fields.Nested(delay_claim),
-    'contactDetails': fields.Nested(contact_details),
-    'payment': fields.Nested(payment_fields),
-}
+import json
 
 class DelayClaimResource(Resource):
 
@@ -41,6 +10,9 @@ class DelayClaimResource(Resource):
         print("starting...")
 
         response = {}
+
+        params = request.json
+        print(params)
 
         return response
 
